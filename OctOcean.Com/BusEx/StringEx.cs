46bb1d461eb6c20990b9173d53fb610ba0171070
smallz <wangyang@samsundot.com>
2018-05-04 09:32:29 +0800

    添加公共扩展类库和共用配置项

---
+++ OctOcean.Com/BusEx/StringEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OctOcean.Com.BusEx
+{
+    /// <summary>
+    /// 字符串相关的扩展方法
+    /// </summary>
+    public static class StringEx
+    {
+        public static int testlength(this string str)
+        {
+            return str.Length;
+
+
+        }
+    }
+}
---
+++ WD.Utils/OctOceanConfig.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WD.Utils
+{
+    public class OctOceanConfig
+    {
+        public OctOceanConfig(string defaultConnectionString, string fileRoot, string urlRoot, string articlePreviewUrl)
+        {
+            this.DefaultConnectionString = defaultConnectionString;
+            this.FileRoot = fileRoot;
+            this.UrlRoot = urlRoot;
+            this.ArticlePreviewUrl = articlePreviewUrl;
+        }
+        public readonly string DefaultConnectionString = string.Empty;
+        public readonly string FileRoot = string.Empty;
+        public readonly string UrlRoot = string.Empty;
+        public readonly string ArticlePreviewUrl = string.Empty;
+
+
+        private string _fileRoot_cache_image;
+        public string FileRoot_Cache_Image
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_fileRoot_cache_image))
+                {
+                    _fileRoot_cache_image = Path.Combine(FileRoot, "Cache/image");
+                    if (!Directory.Exists(_fileRoot_cache_image))
+                    {
+                        Directory.CreateDirectory(_fileRoot_cache_image);
+                    }
+
+                }
+                return _fileRoot_cache_image;
+            }
+
+        }
+
+        public string UrlRoot_Cache_Image
+        {
+            get { return UrlRoot + "/Cache/image"; }
+        }
+
+
+
+
+        private string _fileRoot_web_image;
+        public string FileRoot_Web_Image
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_fileRoot_web_image))
+                {
+                    _fileRoot_web_image = Path.Combine(FileRoot, "Web/image");
+                    if (!Directory.Exists(_fileRoot_web_image))
+                    {
+                        Directory.CreateDirectory(_fileRoot_web_image);
+                    }
+
+                }
+                return _fileRoot_web_image;
+            }
+
+        }
+
+
+
+        public string UrlRoot_Web_Image
+        {
+            get { return UrlRoot + "/Web/image"; }
+        }
+
+
+    }
+}
