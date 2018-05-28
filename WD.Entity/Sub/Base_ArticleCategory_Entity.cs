c14916424e91e12d2fc364d0049c9ccc7878e6ef
octocean26 <octoberocean@163.com>
2018-05-28 18:11:12 +0800

    完善文章类别的维护

--- WD.Entity/Sub/Base_ArticleCategory_Entity.cs
+++ WD.Entity/Sub/Base_ArticleCategory_Entity.cs
@@ -5,13 +5,12 @@ namespace WD.Entity.Sub
     public class Base_ArticleCategory_Entity
     {
         public int Id { get; set; }
-        //[Required,StringLength(10)]
+        
         /// <summary>
         /// 文章类别名称
         /// </summary>
         public string ArticleCategoryName { get; set; }
-        //[Required, StringLength(10)]
-        //[RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")] //限制用户可以输入的字符，吃住只能为字母
+        
         /// <summary>
         /// 文章类别名称对应的Code
         /// </summary>
--- WD.Management.WebSite/Controllers/ArticleCategoryController.cs
+++ WD.Management.WebSite/Controllers/ArticleCategoryController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WD.DataService;
 using WD.DataService.Sub;
+using WD.Management.WebSite.Models;
 
 namespace WD.Management.WebSite.Controllers
 {
@@ -20,16 +21,69 @@ namespace WD.Management.WebSite.Controllers
         public IActionResult Index()
         {
 
-            var all= _base_ArticleCategoryService.GetAllArticleCategory();
+            var all = _base_ArticleCategoryService.GetAllArticleCategory().Select(a => new VM_ArticleCategory()
+            {
+                ArticleCategoryName = a.ArticleCategoryName,
+                ArticleCategoryCode = a.ArticleCategoryCode,
+                Id = a.Id
+            });
 
             return View(all);
         }
 
 
+
         [HttpGet]
-        public IActionResult Edit(int ArticleCategoryId)
+        public IActionResult Edit(int id)
         {
-            return View();
+
+            var _Base_ArticleCategory_Entity = _base_ArticleCategoryService.GetArticleCategory(id);
+
+            VM_ArticleCategory entity = new VM_ArticleCategory()
+            {
+                Id = _Base_ArticleCategory_Entity == null ? 0 : _Base_ArticleCategory_Entity.Id,
+                ArticleCategoryCode = _Base_ArticleCategory_Entity == null ? "" : _Base_ArticleCategory_Entity.ArticleCategoryCode,
+                ArticleCategoryName = _Base_ArticleCategory_Entity == null ? "" : _Base_ArticleCategory_Entity.ArticleCategoryName
+            };
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(VM_ArticleCategory entity)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var _Base_ArticleCategory_Entity = new Entity.Sub.Base_ArticleCategory_Entity()
+                {
+                    Id = entity.Id,
+                    ArticleCategoryCode = entity.ArticleCategoryCode,
+                    ArticleCategoryName = entity.ArticleCategoryName
+                };
+
+                if (entity.Id > 0)
+                {
+                    _base_ArticleCategoryService.UpdateArticleCategory(_Base_ArticleCategory_Entity);
+
+                }
+                else
+                {
+
+                    //创建
+                    entity.Id = _base_ArticleCategoryService.InsertArticleCategory(_Base_ArticleCategory_Entity);
+
+                }
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        public IActionResult Delete(int Id)
+        {
+            _base_ArticleCategoryService.DeleteArticleCategory(Id);
+            return RedirectToAction("Index");
         }
     }
 }
